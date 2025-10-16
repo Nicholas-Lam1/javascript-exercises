3b5307a626b71e65afe8b6cbe4a59719158721f2
Nicholas-Lam1 <nick40831@gmail.com>
2025-10-16 10:07:29 -0600

    Completed exercises 6 to 10

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...arrRemove) {
+  return arr.filter((num) => !arrRemove.includes(num))
 };
 
 // Do not edit below this line
